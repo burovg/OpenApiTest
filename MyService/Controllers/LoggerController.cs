9861cade0dd9d426bf96eb9d4f92560e6c13970f
Gershon Borov <gershon@vcloud.gov.il>
2019-12-31 16:13:22 +0200

    Add project files.

---
+++ MyService/Controllers/LoggerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyService.Models;
+
+namespace MyService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoggerController : ControllerBase
+    {
+        [HttpPost]
+        public LogItem LogData([FromBody]LogItem item)
+        {
+            return item;
+        }
+    }
+}
---
+++ MyService/Models/LogItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyService.Models
+{
+    public class LogItem
+    {
+        public int Id { get; set; }
+        public DateTime LogDate { get; set; }
+        public string Message { get; set; }
+    }
+}
